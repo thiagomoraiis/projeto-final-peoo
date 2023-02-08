ec57ad60e1c75cfbb491570066364c4cc13c252a
Thiago Morais Silva <morais.t@academico.ifrn.edu.br>
2023-02-08 10:22:00 -0300

    falta descrever as linhas

--- telaprincipal.py
+++ telaprincipal.py
@@ -9,45 +9,45 @@ from telaterciaria import TerceiraTela
 
 class PrimeiraTela:
     def __init__(self, mestre=None):
-        self.fram1 = Toplevel()
+        self.fram1 = Toplevel() #criação do widget toplevel
         self.fram1.title("Janela 2")
-        self.fram1["padx"] = 150
-        self.fram1["pady"] = 70
+        self.fram1["padx"] = 150 #preenchimento no eixo x
+        self.fram1["pady"] = 70 #preenchimento no eixo y
 
         self.message1 = Label(
             self.fram1, text="Qual operação deseja realizar??", background="red", foreground="white"
-        )
+        ) #label geral da pagina 
         self.message2 = Label(self.fram1, text="")
         self.message1.pack()
         self.message2.pack()
 
-        self.btn_listar = Button(self.fram1, background="orange", foreground="white")
+        self.btn_listar = Button(self.fram1, background="orange", foreground="white") #botão que executara o listamento dos depositos
         self.btn_listar["text"] = "Historico de depositos"
-        self.btn_listar["command"] = self.listando
+        self.btn_listar["command"] = self.listando # botão receberar a função de listar
         self.btn_listar.pack()
         self.message1.pack()
 
-        self.btn_editar = Button(self.fram1, background="cyan", foreground="white")
+        self.btn_editar = Button(self.fram1, background="cyan", foreground="white") #botão que executara a edição
         self.btn_editar["text"] = "Editar Saldo atual"
-        self.btn_editar["command"] = self.editar
+        self.btn_editar["command"] = self.editar # botão receberar a função de editar
         self.btn_editar.pack()
         self.message1.pack()
 
-        self.btn_deletar = Button(self.fram1, background="cyan", foreground="white")
+        self.btn_deletar = Button(self.fram1, background="cyan", foreground="white") #botão que executara o listamento dos depositos
         self.btn_deletar["text"] = "Cancelar deposito"
-        self.btn_deletar["command"] = self.deletar
+        self.btn_deletar["command"] = self.deletar #botão recebera a função de deletar itens
         self.btn_deletar.pack()
         self.message1.pack()
 
-        self.btn_listar = Button(self.fram1, background="cyan", foreground="white")
+        self.btn_listar = Button(self.fram1, background="cyan", foreground="white") #botão executara a adição dos depositos
         self.btn_listar["text"] = "Fazer novo deposito"
-        self.btn_listar["command"] = self.add
+        self.btn_listar["command"] = self.add #botão executara a adição de depositos
         self.btn_listar.pack()
         self.message1.pack()
 
         self.btn_sair = Button(self.fram1, background="cyan", foreground="white")
         self.btn_sair["text"] = "Sair"
-        self.btn_sair["command"] = self.logout
+        self.btn_sair["command"] = self.logout #função que executar a desconexão do usuario na pagina
         self.btn_sair.pack()
         self.message1.pack()
 
--- telaquartenaria.py
+++ telaquartenaria.py
@@ -2,43 +2,43 @@ from tkinter import *
 from turtle import color
 class QuintaTela:
     def __init__(self, mestre = None):
-        self.top = Toplevel()
-        self.top.title("Atualização")
-        self.fr = Frame(self.top)
-        self.fr["padx"] = 150
-        self.fr["pady"] = 70
-        self.fr.pack()
-        self.lab = Label(self.fr, text="Editar seu saldo", background='orange', foreground='white', )
-        self.lab["font"] = ("Arial", "10", "bold")
+        self.top = Toplevel() #inicialização do widget de nivel superior que sera parametros de outros widgets
+        self.top.title("Atualização") #titulo janeja
+        self.fr = Frame(self.top) #criaçaõ de frame
+        self.fr["padx"] = 150 #preencimento no eixo x (horizontal)
+        self.fr["pady"] = 70 #preencimento do eixo y (vertical)
+        self.fr.pack() #empacotamento do frame que irá exibi-lo na tela 
+        self.lab = Label(self.fr, text="Editar seu saldo", background='orange', foreground='white') #Label para ajudar na orientação do usuario
+        self.lab["font"] = ("Arial", "10", "bold") #definição do estilo das fontes
         self.lab.pack()
         self.fr = Label(self.top, text="Numero do deposito: ")
-        self.fr.pack(side=LEFT)
-        self.inpu1 = Entry(self.top, background='white')
+        self.fr.pack(side=LEFT) #empacotamento do frame e ajustar a posição do label
+        self.inpu1 = Entry(self.top, background='white') #Entry é responsavel por receber os dados do usuario que serão manipulados posteriormente
         self.inpu1.pack(side=LEFT)
         self.fr = Label(self.top, text="Mudar valor:")
         self.fr.pack(side=LEFT)
         self.inpu2 = Entry(self.top, background='white')
         self.inpu2.pack(side=LEFT)
         self.fr = Button(self.top, text='Confirmar',
-        background='#00FF00')
-        self.fr["command"] = self.editar
-        self.fr.pack()
+        background='#00FF00') #inicialização do botao que vai realizar as ações do usuario
+        self.fr["command"] = self.editar #o comando iria receber o botão e, ao clicar, o botão executara o comando da função editar
+        self.fr.pack() #
         self.lab = Label(self.top, text="")
         self.lab.pack()
 
     def editar(self):
-        dindin = []
-        f = open("bd.txt", "r")
-        for linha in f:
-            dindin.append(linha)
-            f.close
-        pos = int(self.inpu1.get())
-        novo = float(self.inpu2.get())
-        dindin[pos] = novo
-        f = open("bd.txt", "w")
-        for linha in dindin:
-            f.write(str(linha))
-            f.write("\n")
-            # float()
-        f.close
-        self.top.destroy()
+        dindin = [] #criação de lista
+        f = open("bd.txt", "r") #variavel que ira abrir o arquivo e ler ele
+        for linha in f: #laço de repetição que ira percorrer as linha do arquivo
+            dindin.append(linha) #função metodo append ira adicionar a variavel "linha" toda vez que p codigo for percorrido  
+            f.close #close ira fechar o arquivo
+        pos = int(self.inpu1.get()) #variavel "pos" ira receber a entrada do primeiro Entry e ira fazer o casting(converter uma variavel para outro tipo),
+                                    # convertendo uma string para um numero inteiro
+        novo = float(self.inpu2.get()) #variavel novo ira converter o segundo Entry em numero de ponto flutuante  
+        dindin[pos] = novo #o indice da lista ira receber o novo valor que o usuario modificar
+        f = open("bd.txt", "w") #variavel ira novamente abrir o arquivo e reescrever o que esta no indice que foi fornecido pelo usuario, editando-o
+        for linha in dindin: #laço de repetição que ira percorrer a lista 
+            f.write(str(linha)) # write ira, de fato, sobrescrever o que o usuario quiser editar 
+            f.write("\n") #quebrar linha
+        f.close 
+        self.top.destroy() #ao clicar no botão, ele ira destruir o frame, voltando para a tela inicial
